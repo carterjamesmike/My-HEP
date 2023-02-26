a63cac15d0a44bba8f4a9ab00c96ca20d0da5354
James Carter <carterjamesmike@gmail.com>
2023-02-26 13:17:00 -0600

    Added Exercise model

--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -1,7 +1,35 @@
 const { Schema, model } = require('mongoose');
 
 const exerciseSchema = new Schema({
-
+    name: { 
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    totalDays: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    url: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    notes: {
+        type: String,
+        trim: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+    
 
 });
 
