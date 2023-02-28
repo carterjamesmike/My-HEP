70bbd261fa3c0fde828a5317583def9436fdd5ad
slthurman01 <slthurman01@gmail.com>
2023-02-27 19:54:32 -0600

    Adds link to exercise page on home page - initial setup of exercise page.

---
+++ client/src/pages/Exercise.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Exercise = () => {
+    return (
+        <div className='text-4xl text-blue-800'>
+            <Link to='/'>Home</Link>
+        </div>
+    )
+    }
+    
