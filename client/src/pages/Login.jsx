164a622a72ff2a4e8664dac9c17121c98fa792d3
slthurman01 <slthurman01@gmail.com>
2023-02-28 19:18:45 -0600

    Creates basic login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function LoginForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  } 
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your username:
+      <input 
+        type="text" 
+        name="username" 
+        value={inputs.username || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter your password:
+        <input 
+          type="text" 
+          name="password" 
+          value={inputs.password || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<LoginForm />);
