ff6ead32e5e337c1a536dcec6e5c904a4e6871bd
James Carter <carterjamesmike@gmail.com>
2023-02-28 19:09:35 -0600

    Changed dependancies in package.json for client and set up Appollo, JWT, and route paths in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import React from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home'
+
+//Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//Construct request middleware that will attach the JWT token to an authorization header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: { 
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
+//Create our apollo client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 
 function App() {
 
 
   return (
-    <div>
-      <Home />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>        
+          <Routes>
+            <Route 
+            path="/" 
+            element={<Home />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider> 
   )
 }
 
