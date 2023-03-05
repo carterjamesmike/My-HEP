507433e327f76d82792449ae3fc805c541f485a1
slthurman01 <slthurman01@gmail.com>
2023-03-05 17:04:25 -0600

    Adds style to signup page.

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -8,35 +8,40 @@ const Signup = () => {
       <div className="flex justify-center items-center bg-blue-100 mt-2">
         <div className="bg-white rounded shadow-2xl p-10">
           <h2 className="text-3xl font-bold mb-2 text-gray-800">
-            Sign up for a profile below
+            Sign Up
           </h2>
-          <p className="text-gray-600 mb-5">Complete the form fields below to create a profile</p>
+
           <form>
+          <p className="text-gray-600 mb-6">
+            Complete the fields below to create a profile
+          </p>
             <div class="grid gap-6 md:grid-cols-1">
+              <div>
               <label
                 for="first_name"
-                class="block text-sm font-medium text-gray-900 dark:gray-800"
+                class="block text-sm font-medium text-gray-900 dark:gray-800 mb-2"
               >
                 First name
               </label>
               <input
+                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
                 type="text"
                 id="first_name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="John"
                 required
               ></input>
+              </div>
               <div>
                 <label
                   for="last_name"
-                  class="block text-sm mb-6 font-medium text-gray-900 dark:gray-800"
+                  class="block text-sm font-medium text-gray-900 dark:gray-800 mb-2"
                 >
                   Last name
                 </label>
                 <input
+                  className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
                   type="text"
                   id="last_name"
-                  class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Doe"
                   required
                 ></input>
@@ -44,14 +49,14 @@ const Signup = () => {
               <div>
                 <label
                   for="user_name"
-                  class="block text-sm mb-6 font-medium text-gray-900 dark:gray-800"
+                  class="block text-sm font-medium text-gray-900 dark:gray-800 mb-2"
                 >
                   Username
                 </label>
                 <input
+                  className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
                   type="text"
                   id="last_name"
-                  class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Username"
                   required
                 ></input>
@@ -60,33 +65,26 @@ const Signup = () => {
               <div>
                 <label
                   for="password"
-                  class="block text-sm mb-6 font-medium text-gray-900 dark:gray-800"
+                  class="block text-sm font-medium text-gray-900 dark:gray-800 mb-2"
                 >
                   Password
                 </label>
                 <input
+                  className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
                   type="text"
                   id="last_name"
-                  class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Password"
                   required
                 ></input>
               </div>
-
-
             </div>
           </form>
-          <p className="text-gray-600">
-            <button class="bg-blue-500 mb-8 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Create a user profile
+          <div className="flex justify-center items-center mt-6">
+            <button
+              className={`bg-blue-100 py-2 px-4 text-sm text-grey-200 rounded border border-green focus:outline-none focus:border-green-dark mb-5`}
+            >
+              Create Profile
             </button>
-            </p>
-          
-          <div className='flex justify-center items-center mb-8'>
-          </div>
-    
-          <div>
-          
           </div>
         </div>
       </div>
