1e2079cfb2c15a262121dae862b3b46abb516260
slthurman01 <slthurman01@gmail.com>
2023-03-06 21:05:00 -0600

    Adds style elements to several pages, adds routing for new pages

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-center items-center bg-blue-100 mt-2">
+      <div className="flex justify-center items-center h-screen bg-blue-100 mt-2">
         <div className="bg-white rounded shadow-2xl p-10">
           <h2 className="text-3xl font-bold mb-2 text-gray-800">Sign Up</h2>
 
