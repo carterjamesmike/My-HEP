3c3d4a7031d2aa5467f8dc2e0701145f38905a84
Seth Daulton <seth.daulton@gmail.com>
2023-03-06 21:16:01 -0600

    Made cosmetic changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <Navbar />
       <div className="flex justify-center items-center h-screen bg-blue-100">
         <div className="bg-white rounded shadow-2xl p-10">
-          <h2 className="text-3xl font-bold mb-2 text-gray-800">My HEP</h2>
+          <h2 className="text-3xl font-bold mb-2">My HEP</h2>
           <p className="text-gray-600">
             <p>Welcome to My HEP - your resource for all things H, E, and P</p>
             <p>Be sure to buy our merch!</p>
